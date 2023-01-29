30bfa2f3fc5bc1ecb93d43d8c2bfa6c0d87f01fa
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-01-29 23:47:11 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={s.container}>
+      <Text style={s.text}>Saku</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+  },
+});
