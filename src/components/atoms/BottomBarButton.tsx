0deb8d99337b692ed991e48394654f86af1f5ad1
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-02-07 09:53:29 +0100

    refactor: add more flexibility to CustomBottomBar

--- src/components/atoms/BottomBarButton.tsx
+++ src/components/atoms/BottomBarButton.tsx
@@ -1,28 +1,69 @@
-import { StyleSheet, Text } from 'react-native';
-import React from 'react';
+import { LayoutChangeEvent, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useMemo } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { Colors } from '@constants/colors';
+import { BottomTabNavigatorRoutes } from '@navigation/types';
+import {
+  HomeFilledIcon,
+  HomeIcon,
+  ClockFilledIcon,
+  ClockIcon,
+  SearchFilledIcon,
+  SearchIcon,
+  LibraryFilledIcon,
+  LibraryIcon,
+} from '@icons';
 
 interface Props {
   label: string;
-  onPress: () => void;
-  icon: JSX.Element;
-  focusedIcon: JSX.Element;
-  focused: boolean;
+  name: string;
+  onPress: (key: string, isFocused: boolean) => void;
+  isFocused: boolean;
+  onLayout: (event: LayoutChangeEvent, name: string) => void;
 }
 
+const getIcon = (name: string, focused: boolean) => {
+  switch (name) {
+    case BottomTabNavigatorRoutes.Home:
+      return focused ? <HomeFilledIcon /> : <HomeIcon />;
+    case BottomTabNavigatorRoutes.Updates:
+      return focused ? <ClockFilledIcon /> : <ClockIcon />;
+    case BottomTabNavigatorRoutes.Search:
+      return focused ? <SearchFilledIcon /> : <SearchIcon />;
+    case BottomTabNavigatorRoutes.Library:
+      return focused ? <LibraryFilledIcon /> : <LibraryIcon />;
+    default:
+      return focused ? <HomeFilledIcon /> : <HomeIcon />;
+  }
+};
+
 const BottomBarButton = ({
-  onPress,
   label,
-  icon,
-  focusedIcon,
-  focused,
+  name,
+  isFocused,
+  onPress,
+  onLayout,
 }: Props) => {
+  const onPressHandler = useCallback(() => {
+    onPress(name, isFocused);
+  }, [isFocused, name, onPress]);
+
+  const onLayoutHandler = useCallback(
+    (event: LayoutChangeEvent) => {
+      onLayout(event, name);
+    },
+    [name, onLayout],
+  );
+
+  const Icon = useMemo(() => getIcon(name, isFocused), [isFocused, name]);
+
   return (
-    <TouchableOpacity style={s.container} onPress={onPress}>
-      {focused ? focusedIcon : icon}
-      <Text style={s.label}>{label}</Text>
-    </TouchableOpacity>
+    <View style={s.container} onLayout={onLayoutHandler}>
+      <TouchableOpacity onPress={onPressHandler} style={s.button}>
+        {Icon}
+        <Text style={s.label}>{label}</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -32,6 +73,10 @@ const s = StyleSheet.create({
   container: {
     height: 54,
     width: 58,
+  },
+  button: {
+    height: '100%',
+    width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
   },
--- src/components/molecules/BottomBar.tsx
+++ src/components/molecules/BottomBar.tsx
@@ -1,23 +1,38 @@
-import { StyleSheet, View } from 'react-native';
-import React, { useMemo } from 'react';
+import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
+import React, { useCallback, useMemo, useReducer } from 'react';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { Colors } from '@constants/colors';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import {
-  ClockFilledIcon,
-  ClockIcon,
-  HomeFilledIcon,
-  HomeIcon,
-  LibraryFilledIcon,
-  LibraryIcon,
-  SearchFilledIcon,
-  SearchIcon,
-} from '@icons';
 import { BottomBarButton } from '@atoms';
-import { BottomTabNavigatorRoutes } from '@navigation/types';
 
-const CustomBottomBar = ({ state, navigation }: BottomTabBarProps) => {
+interface IconPos {
+  name: string;
+  x: number;
+}
+
+const CustomBottomBar = ({
+  state,
+  navigation,
+  descriptors,
+}: BottomTabBarProps) => {
   const { bottom: bottomInset } = useSafeAreaInsets();
+  const [iconsPos, iconsPosDispatch] = useReducer<
+    (prevState: IconPos[], action: IconPos) => IconPos[]
+  >((prevState, { name, x }) => {
+    const index = prevState.findIndex(item => item.name === name);
+
+    if (index === -1) {
+      return [...prevState, { name, x }];
+    }
+
+    return prevState.map((item, i) => {
+      if (i === index) {
+        return { name, x };
+      }
+
+      return item;
+    });
+  }, []);
 
   const positionProps = useMemo(
     () => ({
@@ -26,36 +41,53 @@ const CustomBottomBar = ({ state, navigation }: BottomTabBarProps) => {
     [bottomInset],
   );
 
+  const onLayout = useCallback(
+    (
+      {
+        nativeEvent: {
+          layout: { x },
+        },
+      }: LayoutChangeEvent,
+      name: string,
+    ) => {
+      iconsPosDispatch({ name, x });
+    },
+    [],
+  );
+
+  const onPress = useCallback(
+    (key: string, isFocused: boolean) => {
+      const event = navigation.emit({
+        type: 'tabPress',
+        target: key,
+        canPreventDefault: true,
+      });
+
+      if (!event.defaultPrevented && !isFocused) {
+        navigation.navigate(key);
+      }
+    },
+    [navigation],
+  );
+
   return (
     <View style={[s.container, positionProps]}>
-      <BottomBarButton
-        label={'Home'}
-        onPress={() => navigation.navigate(BottomTabNavigatorRoutes.Home)}
-        icon={<HomeIcon />}
-        focusedIcon={<HomeFilledIcon />}
-        focused={state.index === 0}
-      />
-      <BottomBarButton
-        label={'Updates'}
-        onPress={() => navigation.navigate(BottomTabNavigatorRoutes.Updates)}
-        icon={<ClockIcon />}
-        focusedIcon={<ClockFilledIcon />}
-        focused={state.index === 1}
-      />
-      <BottomBarButton
-        label={'Search'}
-        onPress={() => navigation.navigate(BottomTabNavigatorRoutes.Search)}
-        icon={<SearchIcon />}
-        focusedIcon={<SearchFilledIcon />}
-        focused={state.index === 2}
-      />
-      <BottomBarButton
-        label={'Library'}
-        onPress={() => navigation.navigate(BottomTabNavigatorRoutes.Library)}
-        icon={<LibraryIcon />}
-        focusedIcon={<LibraryFilledIcon />}
-        focused={state.index === 3}
-      />
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label = options.title ?? route.name;
+        const isFocused = state.index === index;
+
+        return (
+          <BottomBarButton
+            key={route.key}
+            label={label}
+            name={route.name}
+            isFocused={isFocused}
+            onPress={onPress}
+            onLayout={onLayout}
+          />
+        );
+      })}
     </View>
   );
 };
