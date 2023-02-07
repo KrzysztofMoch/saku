ab423c434195904fe1f065bf203209c38ddfbc25
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-02-07 13:13:39 +0100

    chore: move renderTabBar to function

--- src/navigation/bottom-tab-navigator.tsx
+++ src/navigation/bottom-tab-navigator.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
+  BottomTabBarProps,
   BottomTabNavigationOptions,
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs';
@@ -15,10 +16,13 @@ import { CustomBottomBar } from '@molecules';
 const Tab = createBottomTabNavigator<BottomTabNavigatorParams>();
 
 export const BottomTabNavigator = () => {
+  const renderTabBar = useCallback(
+    (props: BottomTabBarProps) => <CustomBottomBar {...props} />,
+    [],
+  );
+
   return (
-    <Tab.Navigator
-      screenOptions={screenOptions}
-      tabBar={props => <CustomBottomBar {...props} />}>
+    <Tab.Navigator screenOptions={screenOptions} tabBar={renderTabBar}>
       <Tab.Screen name={BottomTabNavigatorRoutes.Home} component={HomeScreen} />
       <Tab.Screen
         name={BottomTabNavigatorRoutes.Updates}
