e45bbffd12908b666c758c5aed398587a22f0888
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-03-09 14:57:40 +0100

    fix: update `AuthScreen` imports

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
-import { authorize_user } from '@utils/auth';
+import { authorize_user } from '@utils';
 import {
   BottomTabNavigatorRoutes,
   StackNavigatorParams,
