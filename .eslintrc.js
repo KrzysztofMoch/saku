06410fbd201c6020b3d5a6def3e8bb6a4d2e1e0a
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-07-10 00:08:12 +0200

    chore(workspace): fix eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    // we have different babel configs for different parts of the app
+    requireConfigFile: false,
+  },
+  extends: '@react-native',
+};
