4115a0694e5877cd971efd9775d860ab96a07768
KrzysztofMoch <krzysmoch.programs@gmail.com>
2023-07-09 21:36:18 +0200

    feat(mobile/db): setup database

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from '@navigation/stack-navigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import DatabaseProvider from '@nozbe/watermelondb/DatabaseProvider';
 
 import { useAuthStore } from '@store/auth';
 import { useHydration } from '@saku/shared';
@@ -20,9 +21,11 @@ const App = () => {
         <StatusBar barStyle={'light-content'} />
         <GestureHandlerRootView style={s.container}>
           <QueryClientProvider client={queryClient}>
-            <NavigationContainer>
-              {hydrated ? <StackNavigator /> : <PreAppLoadedScreen />}
-            </NavigationContainer>
+            <DatabaseProvider database={database}>
+              <NavigationContainer>
+                {hydrated ? <StackNavigator /> : <PreAppLoadedScreen />}
+              </NavigationContainer>
+            </DatabaseProvider>
           </QueryClientProvider>
         </GestureHandlerRootView>
       </SafeAreaProvider>
